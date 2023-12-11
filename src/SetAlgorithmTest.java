2ff72a137e828d52abae95881a48102e50ab379e
Kaliana Andriamananjara <kandriam@macalester.edu>
2023-12-11 01:41:41 -0600

    Update SetAlgorithmTest

--- src/SetAlgorithmTest.java
+++ src/SetAlgorithmTest.java
@@ -1,12 +1,14 @@
 import java.util.ArrayList;
 
+
 public class SetAlgorithmTest {
     static ArrayList<Card> deck = new ArrayList<Card>();
     static Integer NUMBER_OF_TRAIT_VARIANTS = 3;
+    SetAlgorithm algorithm;
+
 
 
     public static void createDeck() {
-        deck = new ArrayList<>();
         for (int color = 0; color < NUMBER_OF_TRAIT_VARIANTS; color++) {
             for (int number = 0; number < NUMBER_OF_TRAIT_VARIANTS; number++) {
                 for (int shape = 0; shape < NUMBER_OF_TRAIT_VARIANTS; shape++) {
@@ -17,12 +19,19 @@ public class SetAlgorithmTest {
             }
         }
     }
+
+    public ArrayList<ArrayList<Card>>  test1(){
+        ArrayList<Card> startHand = new ArrayList<Card>();
+        ArrayList<ArrayList<Card>> solved = algorithm.detectSets(startHand);
+        return solved;
+    }
     
 
 
     public static void main(String[] args) {
         System.out.println("Testing...");
         createDeck();
+
 
         for (Card card : deck){
             System.out.println(card.getCard());
